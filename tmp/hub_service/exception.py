a856cd21fb22e9130ff4b9510af2d602dd0b030e
道锋潜鳞 <daofengqianlin@mail.hanwuss.com>
2022-08-16 20:21:09 +0800

    隐藏临时插件

--- tmp/hub_service/exception.py
+++
@@ -1,5 +0,0 @@
-class HubServiceNotEnabled(Exception):
-    """HubService 未启用"""
-
-    def __init__(self):
-        super().__init__("HubService 未启用")
--- tmp/manager/util/__init__.py
+++
@@ -1,5 +0,0 @@
-try:
-    from module.hub_service.exception import HubServiceNotEnabled
-    from module.hub_service import hs
-except HubServiceNotEnabled:
-    hs = None
